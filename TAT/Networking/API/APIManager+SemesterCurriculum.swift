d3a438cc25ebc0a78e97e75713ab19295af1b08b
神崎秀吉 <kamisakihideyoshi@outlook.com>
2019-02-09 23:53:04 +0900

    Rename struct and split APIManager+curriculum
    into APIManager+SemesterCurriculum and APIManager+SemesterCurriculumList

---
+++ TAT/Networking/API/APIManager+SemesterCurriculum.swift
@@ -0,0 +1,61 @@
+//
+//  APIManager+SemesterCurriculum.swift
+//  TAT
+//
+//  Created by 神崎秀吉 on 2019/02/09.
+//  Copyright © 2019 jamfly. All rights reserved.
+//
+
+import Moya
+import RxSwift
+import RxCocoa
+import SwiftyJSON
+
+extension APIType {
+  
+  struct SemesterCurriculum: APITargetType {
+    
+    // MARK: - Properties
+    
+    private var studentID: String?
+    private var year: String = ""
+    private var semester: String = ""
+    
+    // MARK: - Initialization
+    
+    init(year: String, semester: String, studentID: String?) {
+      self.year = year
+      self.semester = semester
+      self.studentID = studentID
+    }
+    
+    // MARK: - APITargetType
+    
+    var path: String {
+      return "/api/curriculums"
+    }
+    
+    var method: Moya.Method {
+      return .get
+    }
+    
+    var task: Task {
+      var params: [String: Any] = [
+        "year": year,
+        "sem": semester
+      ]
+      
+      params["targetStudentId"] = studentID
+      
+      return .requestParameters(parameters: params,
+                                encoding: URLEncoding.queryString)
+    }
+  }
+  
+}
+
+extension APIManager {
+  
+  // MARK: - Unwritten yet
+  
+}
---
+++ TAT/Networking/API/APIManager+SemesterCurriculumList.swift
@@ -0,0 +1,53 @@
+//
+//  APIManager+SemesterCurriculumList.swift
+//  TAT
+//
+//  Created by 神崎秀吉 on 2019/02/09.
+//  Copyright © 2019 jamfly. All rights reserved.
+//
+
+import Moya
+import RxSwift
+import RxCocoa
+import SwiftyJSON
+
+extension APIType {
+  
+  struct SemesterCurriculumList: APITargetType {
+    
+    // MARK: - Properties
+    
+    private var studentID: String?
+    
+    // MARK: - Initialization
+    
+    init(with studentID: String?) {
+      self.studentID = studentID
+    }
+    
+    // MARK: - APITargetType
+    
+    var path: String {
+      return "/api/curriculums"
+    }
+    
+    var method: Moya.Method {
+      return .get
+    }
+    
+    var task: Task {
+      var params: [String: Any] = [:]
+      params["targetStudentId"] = studentID
+      
+      return .requestParameters(parameters: params,
+                                encoding: URLEncoding.queryString)
+    }
+  }
+  
+}
+
+extension APIManager {
+  
+  // MARK: - Unwritten yet
+  
+}
